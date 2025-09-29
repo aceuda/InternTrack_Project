480ae70af2934ed8a1bbd3e8c78e3cd831d57c05
Aceucchi <abellaace23@gmail.com>
2025-09-29 23:19:21 +0800

    fix(settings): revise static and templates configuration

--- login/users/views.py
+++ login/users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+def home(request):
+    return render(request, 'users/home.html')
+
+def about(request):
+    return render(request, 'users/about.html')
+
 # Create your views here.
